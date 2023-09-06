40eb2d771f294adb95a48958ac40bd4ae69cf171
Nazir Knuckles <Batking74@gmail.com>
2023-09-06 02:51:37 -0400

    Cleaned up the codebase, and added a main tag in html file.

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -4,17 +4,12 @@ const stopButton = document.getElementById('stop-button');
 const speedInput = document.getElementById('speed');
 const textInput = document.getElementById('text');
 const clearButton = document.getElementById('clear-button');
+const isDisabled = true;
 
-
-
-playButton.addEventListener('click', () => {
-    playText(textInput.value);
-})
+playButton.addEventListener('click', () => playText(textInput.value));
 pauseButton.addEventListener('click', pauseText);
 stopButton.addEventListener('click', stopText);
 clearButton.addEventListener('click', clearText);
-
-const isDisabled = true;
 
 function playText(text) {
     const utterance = new SpeechSynthesisUtterance(text);
@@ -27,27 +22,20 @@ function playText(text) {
     utterance.addEventListener('end', () => {
         textInput.disabled = !isDisabled;
     })
-
-    if (speechSynthesis.speaking) return
-    textInput.disabled = isDisabled
-    speechSynthesis.speak(utterance)
+    
+    if (speechSynthesis.speaking) return;
+    textInput.disabled = isDisabled;
+    speechSynthesis.speak(utterance);
 }
-
-
-
 
 function pauseText() {
-   if (speechSynthesis.speaking) speechSynthesis.pause();
+    if (speechSynthesis.speaking) speechSynthesis.pause();
 }
-
-
 
 function stopText() {
-    speechSynthesis.resume();
+    textInput.disabled = !isDisabled;
     speechSynthesis.cancel();
 }
-
-
 
 function clearText() {
     textInput.value = '';
@@ -55,19 +43,9 @@ function clearText() {
     speechSynthesis.cancel();
 }
 
-
-const commentInput = document.getElementById('comment-input');
-const textLocation = document.getElementById('textLocation');
-const btn = document.getElementById('btn');
-const numComments = document.getElementById('Number-Of-Comments');
-const body = document.getElementById('body');
-const newComment = document.createElement("div");
-
 document.addEventListener('DOMContentLoaded', () => {
-    alert('Welcome to my AI talking computer project!!!');
+    alert("Welcome to Naz's Talking Computer Project!");
 })
-
-// document.onload(body.classList.add('hello'));
 
 commentInput.addEventListener('keydown', (event) => {
     if(event.key === 'Enter') {
